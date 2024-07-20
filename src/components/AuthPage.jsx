2f8beffee421f012e6a651414b85a1d452851c5b
AhmedAleela145 <ahmednuuraleela234@gmail.com>
2024-07-20 10:12:31 +0300

    Auth page

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+// Component for authentication page
+
 const AuthPage = () => {
   return (
     <div>
